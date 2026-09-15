4be3fae7b34e3e91bc2aee65c51210ddd4dd26f4
rubenfloro <112633051+rubenfloro@users.noreply.github.com>
2026-09-15 15:12:36 +0200

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.warptv"
         minSdk = 24
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.3.1"
+        versionCode = 15
+        versionName = "1.3.2"
     }
 
     buildFeatures { buildConfig = true }
